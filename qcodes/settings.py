6d7e1b1211bd90212eacdadcedb4a53c70e2d684
Nqabenhle Mlaba <nqabe@LAPTOP-BPKPP5HE.localdomain>
2022-11-03 22:34:05 +0200

    Modified settings.py

--- qcodes/settings.py
+++ qcodes/settings.py
@@ -16,7 +16,8 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
